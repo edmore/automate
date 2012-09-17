27d852de7e45734fe067214232dfe2b1c66d474b
Edmore Moyo <etmoyo@gmail.com>
2012-09-17 12:57:24 +0200

    refactoring

--- linkify.rb
+++ linkify.rb
@@ -11,8 +11,11 @@ file_extension = ARGV[1] || "rb"
 
 Dir.foreach(folder) do |file|
   file_without_extension = file.partition(".")[0]
-  unless ( File.exist?("/usr/local/bin/" + file_without_extension) )
-    system("sudo ln -s #{folder}/#{file} /usr/local/bin/#{file_without_extension}") if (file.partition(".")[2] == "rb")
+  unless ( File.exist?(link_directory + file_without_extension) )
+    if (file.partition(".")[2] == file_extension)
+      system("sudo ln -s #{folder}/#{file} #{link_directory + file_without_extension}")
+      puts "Created symbolic link for #{file}."
+    end
   end
 end
 
