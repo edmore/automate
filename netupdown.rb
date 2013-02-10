415c847d2544f805d4f6f128a82538d61d2d5c41
Edmore Moyo <etmoyo@gmail.com>
2013-02-10 11:12:23 +0200

    straight vanilla ruby implementation of updown

---
+++ netupdown.rb
@@ -0,0 +1,18 @@
+#! /usr/bin/env ruby
+
+require "net/http"
+require "uri"
+
+uri = URI.parse("http://www.isup.me/#{ARGV[0]}")
+response = Net::HTTP.get_response(uri)
+
+if (response.code == '200')
+  data = Net::HTTP.get(uri)
+  is_match = /is up/.match(data)
+
+  if (is_match)
+    puts "#{ARGV[0]} is UP! Jus you."
+  else
+    puts "#{ARGV[0]} is DOWN dude! Ain't just you."
+  end
+end
