a0680f6ac8bb6b512379b05fd8dad7589a190126
Edmore Moyo <etmoyo@gmail.com>
2013-02-25 14:20:40 +0200

    adding comments to Angie's script

--- talking_angie.sh
+++ talking_angie.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+###############################################################
+# Description - Playback script for my daughter
+#             - stops recording after certain period of silence
+# Author - www.edmoremoyo.com
+###############################################################
+
 rec test.mp3 silence 1 0.3 1% 1 0.3 1% > /dev/null
 play test.mp3 > /dev/null
 rm -f test.mp3
